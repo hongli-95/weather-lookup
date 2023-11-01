36b90da1e4d2e181be1e73ba7bebd7151ae27305
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 <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>
2023-10-31 18:27:34 -0700

    flex wrapped card container

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        //console.log(data);
         setResult(data.results);
       });
     setWeatherResult([]);
